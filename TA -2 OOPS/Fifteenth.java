6165b94beae08842649a51404cc47fd2c4292c60
Karan-14-Chakole <24chakolek@rbunagpur.in>
2025-04-18 05:03:50 -0700

    Add files via upload

---
+++ TA -2 OOPS/Fifteenth.java
@@ -0,0 +1,16 @@
+// Problem: Implement calculator using Lambda expressions.
+ 
+ interface Operation { int op(int a, int b); }
+ 
+ public class Fifteenth {
+     public static void main(String[] args) {
+         Operation add = (a, b) -> a + b;
+         Operation mul = (a, b) -> a * b;
+         System.out.println("Sum: " + add.op(3, 4));
+         System.out.println("Mul: " + mul.op(3, 4));
+     }
+ }
+ /* Output:
+ Sum: 7
+ Mul: 12
+ */
